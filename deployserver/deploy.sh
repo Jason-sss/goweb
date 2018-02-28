3ef59468cd8b78b38ed5fb078d99e3e89dc18ce4
sss <shengss@anystreaming.cn>
2018-02-28 15:58:02 +0800

    edit deploy.sh

--- deployserver/deploy.sh
+++ deployserver/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 killall -9 webserver
-cd /home
-git clone https://github.com/Jason-sss/goweb.git
-cd goweb/webserver
+cd /home/goweb
+git pull
+# git clone https://github.com/Jason-sss/goweb.git
+cd webserver
 ./webserver &
